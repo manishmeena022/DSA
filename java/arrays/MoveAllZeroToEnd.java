741248a538cb517e7f7420089075809b0ab6edcd
Manish  meena <nsit2016manish@gmail.com>
2024-12-17 18:22:17 +0530

    rotate array and move 0 to right

---
+++ java/arrays/MoveAllZeroToEnd.java
@@ -0,0 +1,65 @@
+package DSA.java.arrays;
+
+import java.util.ArrayList;
+
+public class MoveAllZeroToEnd {
+    public static int[] moveAllZeroToEnd(int arr[]){
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] != 0){
+                temp.add(arr[i]);
+            }
+        }
+
+        int nz = temp.size();
+
+        for(int i=0;i<nz;i++){
+            arr[i] = temp.get(i);
+        }
+
+        for(int i=nz;i<arr.length;i++){
+            arr[i] = 0;
+        }
+
+        return arr;
+    }
+    
+    public static int[] movesZeros(int arr[]){
+        int j = -1;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1 ) return arr;
+
+        for(int i=j +1;j<arr.length;i++){
+            if((arr[i] != 0)){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        return arr;
+    }
+
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 0, 2, 3, 0, 4, 0, 1};
+
+        int ans[] = moveAllZeroToEnd(arr);
+        
+        print(ans);
+    }
+    
+}
---
+++ java/arrays/RotateArrayByK.java
@@ -0,0 +1,64 @@
+package DSA.java.arrays;
+
+public class RotateArrayByK {
+
+    public static void rotateArray(int arr[],int n, int k){
+        if(n==0) return; // if array is empty
+
+        k = k %n; // divide it by k to avoid multiple rotations
+
+        if(k > n) return; // check if it different is still greater than length of array
+
+        int [] temp = new int[k];  // to add last k elements in temp array
+
+        for(int i=0;i<k;i++){
+            temp[i] = arr[i];
+        }
+
+        for(int i=0;i< n- k; i++){
+            arr[i] = arr[i + k];
+
+        }
+        for(int i = n -k ;i < n;i++){
+            arr[i] = temp[i - n + k];
+        }
+    }
+
+    public static void Reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+    public static void RotateToLeft(int arr[], int n, int k){
+        if(arr == null || arr.length == 0 || k <=0){
+            return;
+        }
+
+        k = k % n;
+
+        if(k == 0) return;
+        
+        Reverse(arr, 0, n - k-1);
+        Reverse(arr, n - k, n -1);
+        Reverse(arr, 0, n-1);
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2,3,4,5,};
+        int n = arr.length;
+
+        RotateToLeft(arr,n, 4);
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
